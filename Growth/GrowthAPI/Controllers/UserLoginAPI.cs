713f2a655725af40f6f87accd118e6bf04949acb
Kevin Pilatzke <102439098+KevinPilatzke@users.noreply.github.com>
2022-05-26 16:11:37 -0500

    Updated Logic and API

--- Growth/GrowthAPI/Controllers/UserLoginAPI.cs
+++ Growth/GrowthAPI/Controllers/UserLoginAPI.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GrowthBL;
+using GrowthDL;
+using GrowthModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace GrowthAPI.Controllers
 {
-    public class UserLoginAPI : Controller
-    {
-        public IActionResult Index()
+        [Route("[Contoller]")]
+        [ApiController]
+        public class UserLoginAPI : Controller
         {
-            return View();
+            readonly IEFRepo repo;
+            readonly ILogic logic;
+            public UserLoginAPI(IEFRepo repo, ILogic logic)
+            {
+                this.repo = repo;
+                this.logic = logic;
+            }
+            [AllowAnonymous]
+            [HttpPost]
+            [Route("CreateAccount")]
+            [ProducesResponseType(201)]
+            [ProducesResponseType(400)]
+            public ActionResult AddNewAccount([FromQuery][BindRequired] string Username, [BindRequired][DataType(DataType.Password)] string Password, [BindRequired] string Email, string? PhoneNumber)
+            {
+                List<UserAccount> newList = new List<UserAccount>();
+                newList = logic.GetUserAccount
+            }
         }
     }
 }
