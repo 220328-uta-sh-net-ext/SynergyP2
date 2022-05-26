43bd8b39d598e9e1ee405da48909e59a17359a55
cookiemuenster <seafoodgarden@gmail.com>
2022-05-26 09:50:05 -0700

    created goal options component and login feature component

--- GrowthApp/src/app/app.module.ts
+++ GrowthApp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoalsOptionsComponent } from './goals-options/goals-options.component';
+import { LoginFeatureComponent } from './login-feature/login-feature.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoalsOptionsComponent,
+    LoginFeatureComponent
   ],
   imports: [
     BrowserModule,
