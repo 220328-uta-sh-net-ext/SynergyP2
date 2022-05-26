beda3996dd5bb30f36b1d557318ef42cc460be07
Kevin Pilatzke <102439098+KevinPilatzke@users.noreply.github.com>
2022-05-26 16:38:32 -0500

    Fix

--- Growth/GrowthAPI/Controllers/UserLoginAPI.cs
+++ Growth/GrowthAPI/Controllers/UserLoginAPI.cs
@@ -4,29 +4,47 @@ using GrowthModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
 
 namespace GrowthAPI.Controllers
 {
-        [Route("[Contoller]")]
-        [ApiController]
-        public class UserLoginAPI : Controller
+    [Route("[Contoller]")]
+    [ApiController]
+    public class UserLoginAPI : Controller
+    {
+        readonly IEFRepo repo;
+        readonly ILogic logic;
+        public UserLoginAPI(IEFRepo repo, ILogic logic)
         {
-            readonly IEFRepo repo;
-            readonly ILogic logic;
-            public UserLoginAPI(IEFRepo repo, ILogic logic)
+            this.repo = repo;
+            this.logic = logic;
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("CreateAccount")]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        public ActionResult AddNewAccount([FromQuery][BindRequired] string Username, [BindRequired][DataType(DataType.Password)] string Password, [BindRequired] string Email, string? PhoneNumber)
+        {
+            List<UserAccount> newList = new List<UserAccount>();
+            newList = logic.GetUserAccount(Username);
+            if (newList.Count > 0 )
             {
-                this.repo = repo;
-                this.logic = logic;
+                return BadRequest($"{Username} in use, try a different Username");
             }
-            [AllowAnonymous]
-            [HttpPost]
-            [Route("CreateAccount")]
-            [ProducesResponseType(201)]
-            [ProducesResponseType(400)]
-            public ActionResult AddNewAccount([FromQuery][BindRequired] string Username, [BindRequired][DataType(DataType.Password)] string Password, [BindRequired] string Email, string? PhoneNumber)
+            UserAccount newUser = new UserAccount();
+            newUser.Username = Username;
+            newUser.Password = Password;
+            newUser.Email = Email;
+            newUser.PhoneNumber = PhoneNumber;
+            try
             {
-                List<UserAccount> newList = new List<UserAccount>();
-                newList = logic.GetUserAccount
+                repo.AddUser(newUser);
+                return CreatedAtAction("AddNewAccount", newUser);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
             }
         }
     }
