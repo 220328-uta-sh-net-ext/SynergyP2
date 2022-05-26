5daff602275f2b75e01e48051b7f32255108f153
cookiemuenster <seafoodgarden@gmail.com>
2022-05-26 08:30:08 -0700

    in process of setting up routes

--- GrowthApp/src/app/app-routing.module.ts
+++ GrowthApp/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from "@angular/common";
 
-const routes: Routes = [];
+const routes = [{path: '', redirectTo : '/home', pathMatch: 'full'},
+//{path: 'home', component: },
+//{path :}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  declarations: [],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
