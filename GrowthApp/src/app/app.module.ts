3a55651d964aa0ce67b280763fd456b9211174b6
cookiemuenster <seafoodgarden@gmail.com>
2022-05-27 10:03:52 -0700

    deleted old goal component folder /.gitignore

--- GrowthApp/src/app/app.module.ts
+++ GrowthApp/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GoalsOptionsComponent } from './goals-options/goals-options.component';
 import { LoginFeatureComponent } from './login-feature/login-feature.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GoalsOptionsComponent,
     LoginFeatureComponent
   ],
   imports: [
