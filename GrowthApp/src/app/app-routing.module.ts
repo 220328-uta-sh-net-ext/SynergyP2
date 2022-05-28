484a9fd1e3c66c0f49eda940f295778724ff6179
blake518@revature.net <blake518@revature.net>
2022-05-27 20:44:28 -0700

    successfully routed from landing page to log in

--- GrowthApp/src/app/app-routing.module.ts
+++ GrowthApp/src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, ROUTES } from '@angular/router';
-import { CommonModule } from "@angular/common";
+// { CommonModule } from "@angular/common";
+import { GoalOptionsComponent } from './goal-options/goal-options.component';
+import { LoginFeatureComponent } from './login-feature/login-feature.component';
 
-const routes =  [{path: 'src/app/login-feature', redirectTo : '/login-feature', pathMatch: 'full'},
-//{path: 'home', component: },
+const routes = [
+   //{ path: 'src/app/login-feature', redirectTo: '/login-feature', pathMatch: 'full' },
+{path: 'login', component: LoginFeatureComponent}
 //{path :}
 ];
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, RouterModule.forRoot(routes)],
+  //declarations: [],
+  //imports: [CommonModule, RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
