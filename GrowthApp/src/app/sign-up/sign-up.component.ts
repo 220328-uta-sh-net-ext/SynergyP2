6cd1015aa20ca4a58f35df6e01e91a72002cdd79
blake518@revature.net <blake518@revature.net>
2022-05-28 08:59:23 -0700

    added template for sign up page

--- GrowthApp/src/app/sign-up/sign-up.component.ts
+++ GrowthApp/src/app/sign-up/sign-up.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
+  username: string = "";
+  password: string = "";
 
-  constructor() { }
+  constructor()
+  {
+    console.log("sign up page constructed")
+  }
+  onSubmit(): void
+  {
+    console.log(this.username, this.password);
+  }
 
-  ngOnInit(): void {
+  ngOnInit(): void
+  {
+    console.log("sign up component initialized");
   }
 
 }
