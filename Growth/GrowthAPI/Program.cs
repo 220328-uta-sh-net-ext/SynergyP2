1ff936f917bdc751aed107fa154f6185ef7dfa94
Kevin Pilatzke <102439098+KevinPilatzke@users.noreply.github.com>
2022-05-31 14:44:58 -0500

    fixed xml commits

--- Growth/GrowthAPI/Program.cs
+++ Growth/GrowthAPI/Program.cs
@@ -78,6 +78,7 @@ if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 
 
 app.UseHttpsRedirection();
+app.Logger.LogInformation("App Started");
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseStaticFiles();
