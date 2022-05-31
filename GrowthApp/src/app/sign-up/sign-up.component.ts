4adb49eed888750587cb97eeb6924d0f9570e961
cookiemuenster <seafoodgarden@gmail.com>
2022-05-30 18:58:00 -0700

    updating my branch with Blake's additions

--- GrowthApp/src/app/sign-up/sign-up.component.ts
+++ GrowthApp/src/app/sign-up/sign-up.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['./sign-up.component.css'],
 })
 export class SignUpComponent implements OnInit {
+  username: string = '';
+  password: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    console.log('sign up page constructed');
   }
 
+  onSubmit(): void {
+    console.log(this.username, this.password);
+  }
+
+  ngOnInit(): void {
+    console.log('sign up component initialized');
+  }
 }
