1b1b70eb3d51b3e13d57d795f75bf0fe720b1a7e
cookiemuenster <seafoodgarden@gmail.com>
2022-05-31 09:33:41 -0700

    added services folder with templates/added modules

--- GrowthApp/src/app/app.module.ts
+++ GrowthApp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -19,13 +20,10 @@ import { ColorpaletteComponent } from './colorpalette/colorpalette.component';
     SignUpComponent,
     UserDashboardComponent,
     LandingPageComponent,
-    ColorpaletteComponent
+    ColorpaletteComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
