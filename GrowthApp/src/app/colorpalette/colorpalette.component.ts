39e4a83f4fce3086a0d83be386bdbe4b2e3226eb
cookiemuenster <seafoodgarden@gmail.com>
2022-06-01 07:46:13 -0700

    BUTTON!!!

--- GrowthApp/src/app/colorpalette/colorpalette.component.ts
+++ GrowthApp/src/app/colorpalette/colorpalette.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Chart, registerables } from 'chart.js';
-Chart.register(...registerables);
 
 @Component({
   selector: 'app-colorpalette',
