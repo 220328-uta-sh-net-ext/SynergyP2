ef0848ee29dc55c5f5def952a129f914b1278c34
cookiemuenster <seafoodgarden@gmail.com>
2022-05-31 17:44:23 -0700

    package.json additions and chart build failure

--- GrowthApp/src/app/colorpalette/colorpalette.component.ts
+++ GrowthApp/src/app/colorpalette/colorpalette.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-colorpalette',
   templateUrl: './colorpalette.component.html',
-  styleUrls: ['./colorpalette.component.css']
+  styleUrls: ['./colorpalette.component.css'],
 })
 export class ColorpaletteComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
